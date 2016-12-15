72589b9842124e3493001c2338ac49c5b1f5257f
speed <k78@qq.com>
2016-12-15 16:20:04 +0800

    Update and rename ubuntu to Ubuntu.sh

---
+++ Ubuntu.sh
@@ -0,0 +1,78 @@
+#================================================
+# Ubuntu-16.04-x86_64
+#================================================
+
+
+#================================================
+# Customize sources for apt-get
+#================================================
+
+echo "deb http://archive.ubuntu.com/ubuntu xenial main universe" > /etc/apt/sources.list \
+  && echo "deb http://archive.ubuntu.com/ubuntu xenial-updates main universe" >> /etc/apt/sources.list \
+  && echo "deb http://security.ubuntu.com/ubuntu xenial-security main universe" >> /etc/apt/sources.list
+
+#========================
+# Miscellaneous packages
+# Includes minimal runtime used for executing non GUI Java programs
+#========================
+
+apt-get update -qqy \
+  && apt-get -qqy --no-install-recommends install \
+    bzip2 \
+    ca-certificates \
+    openjdk-8-jre-headless \
+    sudo \
+    unzip \
+    wget \
+  && rm -rf /var/lib/apt/lists/* /var/cache/apt/* \
+  && sed -i 's/securerandom\.source=file:\/dev\/random/securerandom\.source=file:\/dev\/urandom/' /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/java.security
+
+#==========
+# Selenium
+#==========
+mkdir -p /opt/selenium \
+  && wget --no-verbose https://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar -O /opt/selenium/selenium-server-standalone.jar
+
+#========================================
+# Add normal user with passwordless sudo
+#========================================
+sudo useradd seluser --shell /bin/bash --create-home \
+  && sudo usermod -a -G sudo seluser \
+  && echo 'ALL ALL = (ALL) NOPASSWD: ALL' >> /etc/sudoers \
+  && echo 'seluser:secret' | chpasswd
+  
+#==============
+# VNC and Xvfb
+#==============
+
+apt-get update -qqy \
+  && apt-get -qqy install \
+    xvfb \
+  && rm -rf /var/lib/apt/lists/* /var/cache/apt/*
+
+  
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+  && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list \
+  && apt-get update -qqy \
+  && apt-get -qqy install google-chrome-stable \
+  && rm /etc/apt/sources.list.d/google-chrome.list \
+  && rm -rf /var/lib/apt/lists/* /var/cache/apt/*
+  
+  
+wget --no-verbose -O /tmp/chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/2.25/chromedriver_linux64.zip \
+  && rm -rf /opt/selenium/chromedriver \
+  && unzip /tmp/chromedriver_linux64.zip -d /opt/selenium \
+  && rm /tmp/chromedriver_linux64.zip \
+  && chmod 755 /opt/selenium/chromedriver \
+  && ln -fs /opt/selenium/chromedriver /usr/bin/chromedriver
+
+chmod +x /opt/google/chrome/google-chrome
+chown -R seluser:seluser /opt/selenium
+
+su seluser
+
+nohup xvfb-run -n 99 --server-args="-screen 0 1280x1024x24 -ac +extension RANDR" java -jar /opt/selenium/selenium-server-standalone.jar  -role hub &
+
+nohup xvfb-run -n 98 --server-args="-screen 0 1280x1024x24 -ac +extension RANDR" java -Dwebdriver.chrome.driver=/opt/selenium/chromedriver -jar /opt/selenium/selenium-server-standalone.jar  -role node -hub http://localhost:4444/grid/register/ -nodeConfig /opt/selenium/config.json &
+
+
